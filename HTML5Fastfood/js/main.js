06fbdff8bbaeb5e8b31d173ef09bb5cc3e812b71
johnbruin <john__000@LENOVO-PC>
2017-04-23 13:38:50 +0200

    Added the whole project

---
+++ HTML5Fastfood/js/main.js
@@ -0,0 +1,166 @@
+﻿$(document).ready(function () {
+
+    var _rolls = [];
+    var _score = 0;
+    var _description;
+    var _prize = 0;
+    var _hasSpinned = false;
+
+    function start() {
+        init();
+        draw();
+    }
+
+    var delay = (function () {
+        var timer = 0;
+        return function (callback, ms) {
+            clearTimeout(timer);
+            timer = setTimeout(callback, ms);
+        };
+    })();
+
+    var mainCanvas;
+    var mainContext;
+    function init() {
+
+        _score = 2500;
+
+        mainCanvas = document.getElementById('canvasMain');
+        mainContext = mainCanvas.getContext('2d');
+        $('#canvasMain').css('background-color', '#000000');
+
+        _rolls.push(new roll("1,6,2,3,4,5,6,4"));
+        _rolls.push(new roll("6,5,4,3,6,2,1,4"));
+        _rolls.push(new roll("3,4,6,2,1,6,5,4"));
+
+        $("#startButton").click(function () {
+            if (_score == 0) {
+                _score = 2500;
+            }
+            _score = _score - 25;
+
+            _description = "3x = WIN";
+
+            _rolls[0].Start();
+            _rolls[1].Start();
+            _rolls[2].Start();
+
+            var delayFactor = 50;
+            delay(function () {
+                _rolls[0].Stop();
+
+                delay(function () {
+                    _rolls[1].Stop();
+
+                    delay(function () {
+                        _rolls[2].Stop();
+                    }, delayFactor * (Math.floor(Math.random() * 15) + 10));
+
+                }, delayFactor * (Math.floor(Math.random() * 15) + 10));
+
+            }, delayFactor * (Math.floor(Math.random() * 15) + 10));
+            _hasSpinned = true;
+        });
+    }
+
+    Number.prototype.pad = function (size) {
+        var s = String(this);
+        while (s.length < (size || 2)) { s = "0" + s; }
+        return s;
+    }
+
+    var then = Date.now();
+    var fps = 40;
+
+    var _red = 255;
+    var _red_add = -2;
+    var _green = 255;
+    var _green_add = -5;
+    var _blue = 255;
+    var _blue_addd = -5;
+    function draw() {
+        requestAnimationFrame(draw);
+
+        var now = Date.now();
+        var delta = now - then;
+        var interval = 1000 / fps;
+        if (delta > interval) {
+            then = now - (delta % interval);
+
+            mainContext.drawImage(_rolls[0].Draw(), 10, 10, 100, 300);
+            mainContext.drawImage(_rolls[1].Draw(), 120, 10, 100, 300);
+            mainContext.drawImage(_rolls[2].Draw(), 230, 10, 100, 300);
+        }
+
+        if (_prize > 0)
+        {
+            _hasSpinned = false;
+            $("#startButton").prop("disabled", true);
+            _score = _score + 1;
+            _prize = _prize - 1;
+
+            if (_red > 255 || _red < 248) {
+                _red_add = -_red_add;
+            }
+            _red = _red - _red_add;
+
+            if (_green > 255 || _green < 30) {
+                _green_add = -_green_add;
+            }
+            _green = _green - _green_add;
+
+            if (_blue > 255 || _blue < 150) {
+                _blue_addd = -_blue_addd;
+            }
+            _blue = _blue - _blue_addd;
+
+            $('#win').css('color', 'rgb(' + _red + ', ' + _green + ', ' + _blue + ')');
+        }
+        else
+        {
+            $('#win').css('color', 'rgb(255, 255, 255)');
+            $("#startButton").prop("disabled", false);
+            if (_score <= 0) {
+                _description = "GAME OVER";
+                _score = 0;
+            }
+            else {
+                if (_hasSpinned && _rolls[0].Result() == _rolls[1].Result() && _rolls[1].Result() == _rolls[2].Result()) {
+                    switch (_rolls[0].Result()) {
+                        case '1':
+                            _description = "Big Mac 495 kcal";
+                            _prize = 495;
+                            break;
+                        case '2':
+                            _description = "Filet-O-Fish 350 kcal";
+                            _prize = 350;
+                            break;
+                        case '3':
+                            _description = "Medium Frites 340 kcal";
+                            _prize = 340;
+                            break;
+                        case '4':
+                            _description = "Mc Nuggets 250 kcal";
+                            _prize = 250;
+                            break;
+                        case '5':
+                            _description = "Milkshake 355 kcal";
+                            _prize = 355;
+                            break;
+                        case '6':
+                            _description = "Sundae Ice 275 kcal";
+                            _prize = 275;
+                            break;
+                    }
+                }
+                else {
+                    _description = "3x = WIN";
+                }
+            }
+        }
+        $("#score").html(_score.pad(6) + "<br />kcal");
+        $("#win").html(_description);
+    }
+
+    start();
+});
---
+++ HTML5Fastfood/js/roll.js
@@ -0,0 +1,94 @@
+﻿function roll(stringParm) {
+
+    var _canvas = document.createElement("canvas");
+    _canvas.height = 300;
+    _canvas.width = 100;
+    var _context = _canvas.getContext("2d");
+
+    var _y = 0;
+    var _yadd = 0;
+    var _isStopped = true;
+    var _result = '0';
+    var _visibleImages = [];
+    var _index = 0;
+    var _winLine = 0;
+
+    var createImage = function (src) {
+        var img = new Image();
+        img.src = src;
+        return img;
+    }
+
+    var _overlayTopImage = createImage("images/overlay_top.png");
+    var _overlayBottomImage = createImage("images/overlay_bottom.png");
+
+    var _imagesString = stringParm.split(',');
+    var _images = new Array();
+    for (var i = 0; i < _imagesString.length; i++) {
+        _images.push(createImage("images/" + _imagesString[i] + ".png"));
+    }
+
+    this.Result = function () {
+        return _result;
+    }
+
+    this.Stop = function () {
+        _isStopped = true;
+    }
+
+    this.Start = function () {
+        _result = '0';
+        _isStopped = false;
+        _yadd = 25;
+        _y = -_images[0].height;
+    }
+    
+    this.Draw = function () {
+
+        _context.clearRect(0, 0, _canvas.width, _canvas.height);
+
+        _y = _y + _yadd;
+
+        if (_y >= 0) {
+            if (_isStopped) {
+                _yadd = 0;
+            }
+            _y = -_images[0].height;
+
+            _index--;
+            if (_index < 0)
+                _index = _imagesString.length - 1;
+            var index0 = _index - 1;
+            if (index0 < 0)
+                index0 = _imagesString.length - 1;
+            var index3 = _index + 2;
+            if (index3 > _imagesString.length - 1) {
+                index3 = 0;
+            }
+            var index2 = _index + 1;
+            if (index2 > _imagesString.length - 1) {
+                index2 = 0;
+                index3 = 1;
+            }
+            _winLine = index2;
+            
+            _visibleImages[0] = _images[index0];
+            _visibleImages[1] = _images[_index];
+            _visibleImages[2] = _images[index2];
+            _visibleImages[3] = _images[index3];
+        }        
+
+        for (var i = 0; i < _visibleImages.length; i++) {
+            var y = _y + (i * _visibleImages[i].height);
+            _context.drawImage(_visibleImages[i], 0, y);
+            if (y == _canvas.height / 3 && _yadd == 0 && _isStopped) {
+                _result = _imagesString[_winLine];
+            }
+        }
+
+        _context.drawImage(_overlayTopImage, 0, 0);
+        _context.drawImage(_overlayBottomImage, 0, 200);
+
+        return _canvas;
+    }
+}
